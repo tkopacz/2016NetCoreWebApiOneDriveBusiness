a4696316314e706939eb79296ac9f672c39491b7
Tomasz Kopacz <tkopacz@tomaszkopacz.com>
2016-11-19 23:27:42 +0100

    Working - openid + bearer by hand

--- WOneDriveREST/Startup.cs
+++ WOneDriveREST/Startup.cs
@@ -13,11 +13,12 @@ using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using Microsoft.Identity.Client;
-using WOneDriveREST.Helper;
 using System.IdentityModel.Tokens.Jwt;
 using System.Net.Http;
 using System.Text;
 using System.Text.Encodings.Web;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
 
 namespace WOneDriveREST
 {
@@ -111,73 +112,44 @@ namespace WOneDriveREST
 
         private async Task CodeReceived(AuthorizationCodeReceivedContext arg) {
             var code = arg.ProtocolMessage.Code;
-
+            //By Hand! - simplest call
             HttpClient clt = new HttpClient();
-            //clt.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", "TRmohJkVTKoufpbcxmdyRv0");
             clt.BaseAddress = new Uri("https://login.microsoftonline.com/common/oauth2/v2.0/token");
-            /*
-Accept: application/json
-x-client-SKU: MSAL.Desktop
-x-client-Ver: 1.0.0.0
-x-client-CPU: x64
-x-client-OS: Microsoft Windows NT 10.0.14393.0
-client-request-id: b461dc4d-c545-40e1-bb31-7149753413c6
-return-client-request-id: true
-Content-Type: application/x-www-form-urlencoded
-Host: login.microsoftonline.com
-Content-Length: 1504
-Expect: 100-continue
-
-            
-scope=openid+email+profile+offline_access&
-client_id=8a59a106-9a87-4331-98d9-65c34d6392d0&
-client_secret=TRmohJkVTKoufpbcxmdyRv0&
-grant_type=authorization_code&
-redirect_uri=http%3A%2F%2Flocalhost%3A2935%2Fsignin-oidc&
-code=OAQABAAAAAADRNYRQ3dhRSrm-4K-adpCJ4FGhjhbI-Vy_FsjmcrPA8cYIiVSxviB-kndVA7zdLyjAUreLnL8tQQ550pQold4-M3cR0Xifjjk2K6wgkQjylCikrwqYg9039M_OgixDhzOKI-jc83b4SIRtjSB9Gz5bHR36AGVppaHQMbxbkyYb4Swx3p12mmE0ypvJqloFZ0AL75fexMzLUvpgD91ZDkNRUwn_ErqKlaNwH5b6BgiXitfk4iRb0BXP7m-NdUoVtVl7qnKREztO5A-cKEOVkHS5VQq5cJEUay3jEhs_fj_IlefPvx1KGHg08v6nGmj5VjhRDynCnrT2SpFiiuDPTIpmDrxTKOuStNJxwsREnLfeVcawGdKanW5Je7BayCwiaO0bwSvxnNgn1lk-yaXMLo6yQwRdJfeaa2nsyklFUMnJPcUCZPBNXO_xq7ka11BeRO7Z3I6nmaUwOLsI7yvC02ss_6xDmVZ26lDIEjP9vCISWBzIBUSlZ9IElRGyvKctMYb0ZLHtkadBVmBoEqzK4b03zrlbObwOBF9HFdQDv0QGD9_2WrRZTedgZvaF27GVuPTihLWkcp8h-L-Fu-77LnnFDjkuOobmBG2a33wsI1BnqEhgmPgrF0Vy6a9MHqkIeglOXgvL3QwP1DtdJd11vXW56RoDQBnUxOM-Nnj95-uqnD0BcKQYswDasn4RKLQQiscsF4mfcLACGR2vH6M4gtIOMre5U25DyVisC0TmY0zQnmy17PkI4PnIbZ1mna5H-FsZpPVPq7YrbFI8AzvlbaR0MhAf0PVYpH5SznRhddI7tvoI8I3QvRMNvzIV9E554Wxh38ZMD_0wizElollw59cf5Jq595ypmVTRvFe7yeJeOHWg0sEomeRHXTrS28xso6b4MsuXIbnryv-spBkdx3v4FDzx9grpkwZGLDsqKHIRAa9bu8lQukcallYZVfC7ZGtPwBR4-EiKJMPPUCZh4WQyR95IgDpewGbiAMXm__rhmddlFnptlitfTXg2yXZ6rwPpEIo-PwRGNEh1G7mV526NVWi_g3-Ng5kODLwQWWZTaLX3e5-A1zomyVOOLr-tTgyRyiu00bcGh7nxGz0C9kFnAcMlM4UAp7FqORt4eqsBllUKzsCsqXeNiGYdoVkoI3rqklfDTZjQ-RODT1UJKY6HNIvDJ8qQuxeX-Vx38grzxsN_EyYs6Dss9BrU479gaF27FQK3a9A7iP6tIIHrndL5Px6xwPOJctDTCvd8K42JCCAA
-
-            */
-            //clt.DefaultRequestHeaders.Con
             List<Param> lst = new List<WOneDriveREST.Param>();
             lst.Add(new Param { Name = "grant_type", Value = "authorization_code" });
-            //lst.Add(new Param { Name = "redrect_uri", Value = "http://localhost:2935" });
-            lst.Add(new Param { Name = "redrect_uri", Value = "http%3A%2F%2Flocalhost%3A2935" });
-            lst.Add(new Param { Name = "response_mode", Value = "form_post" });
-            lst.Add(new Param { Name = "state", Value = "abc" });
-            lst.Add(new Param { Name = "nonce", Value = "1234" });
+            lst.Add(new Param { Name = "redirect_uri", Value = "http%3A%2F%2Flocalhost%3A2935%2Fsignin-oidc" });
+            lst.Add(new Param { Name = "scope", Value = "openid+email+profile+offline_access" });
             lst.Add(new Param { Name = "client_id", Value = "8a59a106-9a87-4331-98d9-65c34d6392d0" });
             lst.Add(new Param { Name = "client_secret", Value = "TRmohJkVTKoufpbcxmdyRv0" });
             lst.Add(new Param { Name = "code", Value = code });
             StringBuilder sb = new StringBuilder();
-            var enc = HtmlEncoder.Create();
             for (int i = 0; i < lst.Count; i++)
             {
                 var item = lst[i];
                 sb.Append(item.Name); sb.Append('=');
                 sb.Append(item.Value);
-                //sb.Append(enc.Encode(item.Value));
                 if (i < lst.Count - 1) sb.Append('&');
             }
             var cnt = new StringContent(sb.ToString());
             cnt.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/x-www-form-urlencoded");
             var result = await clt.PostAsync("", cnt);
+            JObject j = JObject.Parse(await result.Content.ReadAsStringAsync());
+            var bearer = j["access_token"].ToString();
 
-            //string signedUserId = arg.Ticket.Principal.FindFirst(ClaimTypes.NameIdentifier).Value;
-            //ConfidentialClientApplication cca;
-            //cca = new ConfidentialClientApplication(
-            //        "8a59a106-9a87-4331-98d9-65c34d6392d0",
-            //        "http://localhost:2935",
-            //        new ClientCredential("TRmohJkVTKoufpbcxmdyRv0"),
-            //        new SessionTokenCache(signedUserId, arg.HttpContext)
-            //    );
-            //string[] scopes = { "User.Read", "Mail.Send", "User.ReadWrite", "openid", "email", "profile", "offline_access" };
-            //AuthenticationResult result = await cca.AcquireTokenByAuthorizationCodeAsync(scopes, code);
+            clt = new HttpClient();
+            clt.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", bearer);
+            clt.BaseAddress = new Uri("https://graph.microsoft.com");
+            result = await clt.GetAsync("v1.0/me");
+            Debug.WriteLine(await result.Content.ReadAsStringAsync());
+
+
         }
 
         private async Task OnToken(TokenValidatedContext arg) {
         }
 
         private async Task OnAuthenticationFailed(FailureContext arg) {
+            //Error from OpenID framework!
         }
     }
 }
