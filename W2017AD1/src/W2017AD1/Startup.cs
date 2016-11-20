e420e52911ee3a3697c2fbbd480963223b93b02e
Tomasz Kopacz <tkopacz@tomaszkopacz.com>
2016-11-20 15:29:54 +0100

    Work

---
+++ W2017AD1/src/W2017AD1/Startup.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using W2017AD1.Data;
+using W2017AD1.Models;
+using W2017AD1.Services;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.AspNetCore.Authentication;
+using W2017AD1.Code;
+
+namespace W2017AD1
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            if (env.IsDevelopment())
+            {
+                // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
+                builder.AddUserSecrets("aspnet-W2017AD1-7fe1ae21-0395-4abb-a50a-1bfb995c5f5b");
+            }
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddMvc();
+
+            // Add application services.
+            services.AddTransient<IEmailSender, AuthMessageSender>();
+            services.AddTransient<ISmsSender, AuthMessageSender>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseIdentity();
+
+            //  Update-Database before!
+            // Old AD v1: https://portal.azure.com/#blade/Microsoft_AAD_IAM/ApplicationBlade/objectId/68945602-4e76-4aef-858d-5e35a85c0a5f/appId/39968e99-b355-4564-87d5-61f21fc54e5b
+            // Add external authentication middleware below. To configure them please see http://go.microsoft.com/fwlink/?LinkID=532715
+            app.UseOpenIdConnectAuthentication(new OpenIdConnectOptions
+            {
+                ClientId = "39968e99-b355-4564-87d5-61f21fc54e5b",
+                ClientSecret = "9WuOAgd13PMssfw+CQH7W9pXo2iUtNbsuaa9N25eUV8=",
+                PostLogoutRedirectUri = "http://localhost:4104/",
+                Authority = "https://login.microsoftonline.com/common",
+                Scope = { "User.Read", "Mail.Send", "User.ReadWrite", "openid", "email", "profile", "offline_access" },
+                ResponseType = OpenIdConnectResponseType.CodeIdToken,
+                TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+                {
+                    IssuerValidator = (issuer, token, tvp) =>
+                    {
+                        if (CheckTenant(issuer, token))
+                            return issuer;
+                        else
+                            throw new SecurityTokenInvalidIssuerException("Invalid issuer");
+                    },
+                },
+                Events = new OpenIdConnectEvents
+                {
+                    OnRemoteFailure = OnAuthenticationFailed,
+                    OnTokenValidated = OnToken,
+                    OnAuthorizationCodeReceived = CodeReceived
+                }
+
+            });
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+        private async Task CodeReceived(AuthorizationCodeReceivedContext context)
+        {
+            var request = context.HttpContext.Request;
+            var currentUri = UriHelper.BuildAbsolute(request.Scheme, request.Host, request.PathBase, request.Path);
+            var credential = new ClientCredential("7893fa6a-0222-4fc9-b3d8-0f2f7395f208", "yqLbazd5UcGbzjaFg8H2Hbf");
+            var authContext = new AuthenticationContext("https://login.microsoftonline.com/common/v2.0", AuthPropertiesTokenCache.ForCodeRedemption(context.Properties));
+            var resource = "openid+email+profile+offline_access"; //NO: This is for AD v1: "https://graph.windows.net";
+
+            var result = await authContext.AcquireTokenByAuthorizationCodeAsync(
+                context.ProtocolMessage.Code, new Uri(currentUri), credential, resource);
+
+            context.HandleCodeRedemption();
+        }
+
+        private async Task OnToken(TokenValidatedContext arg)
+        {
+        }
+
+        private async Task OnAuthenticationFailed(FailureContext arg)
+        {
+        }
+
+        private bool CheckTenant(string issuer, SecurityToken token)
+        {
+            //For Example - ususally DB or 
+            if (issuer != "https://login.microsoftonline.com/72f988bf-86f1-41af-91ab-2d7cd011db47/v2.0") return false;
+            return true;
+        }
+    }
+}
