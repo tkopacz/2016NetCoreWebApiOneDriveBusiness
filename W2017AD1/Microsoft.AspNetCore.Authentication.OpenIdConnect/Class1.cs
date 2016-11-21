d61eb3e0e361b8a318b40d37d2e7ee19624c1141
Tomasz Kopacz <tkopacz@tomaszkopacz.com>
2016-11-21 15:08:57 +0100

    Work

--- W2017AD1/Microsoft.AspNetCore.Authentication.OpenIdConnect/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Microsoft.AspNetCore.Authentication.OpenIdConnect
-{
-    public class Class1
-    {
-    }
-}
--- W2017AD1/Microsoft.AspNetCore.Authentication/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Microsoft.AspNetCore.Authentication
-{
-    public class Class1
-    {
-    }
-}
